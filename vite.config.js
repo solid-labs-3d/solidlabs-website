8641213adc969271ec1059701020d1be12da7c09
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2026-03-03 18:38:27 +0530

    [fix] reverted some changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'/',
+  base:'/solidlabs-website/',
   plugins: [react()],
   resolve: {
     alias: { '@': '/src' }
